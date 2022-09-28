bd87e99a5e3032601e2baebea1f07c76a5cce306
ExpertZs <zs.freelancer1@gmail.com>
2022-09-28 18:21:34 +0600

    Update simple Calculator

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,24 @@
-# To take input Numbers from user
-puts "Please Enter A Number:"  # To print the dialog to enter Number
-num1= gets.chomp().to_f            # To store input data in a variable
+# To take input  from user
+puts "Please Enter A Number:"        # To print the dialog to enter Number
+num1= gets.chomp().to_f              # To store input data in a variable
+
+puts "Please Enter A operator:"      # To print the dialog to enter operator
+operator= gets.chomp()               # To store input data in a variable
 
 puts "Please Enter Another Number:"  # To print the dialog to enter Number
-num2= gets.chomp().to_f            # To store input data in a variable
+num2= gets.chomp().to_f              # To store input data in a variable
 
-puts (num1 + num2)                     # To print stored data in a variable
+
+if operator == "+"                    # If condition Started
+  puts (num1 + num2)                  # return sum value
+elsif operator == "-"                 # else if condition Started
+  puts (num1 - num2)                  # return subtraction value
+elsif operator == "*"                 # else if condition Started
+  puts (num1 * num2)                  # return multiplication value
+elsif operator == "/"                 # else if condition Started
+  puts (num1 / num2)                  # return division value
+elsif operator == "%"                 # else if condition Started
+  puts (num1 % num2)                  # return reminder value
+else                                  # else  condition
+  puts "You Enter a wrong operator"   # return wrong operator
+end                                   # End of IF else condition
