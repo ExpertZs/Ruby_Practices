d198d32dad7f911a3adcb73ca3a0da7347f9c04b
ExpertZs <zs.freelancer1@gmail.com>
2022-09-28 21:26:03 +0600

    Initialize Method

--- class_object_inheritance_others.rb
+++ class_object_inheritance_others.rb
@@ -14,3 +14,18 @@ book2.author="bassam & saiyera"
 book2.pages=400
 
 puts book2.author       # Printing object value
+
+# Creating Class to implement Initialize Method
+class Cars
+  attr_accessor :name, :brand, :made_in
+  def initialize(name, brand, made_in)    # Method to initialize object
+    @name = name
+    @brand = brand
+    @made_in = made_in
+  end
+end
+
+car1 = Cars.new("Tata nano", "Tata motors", "India")                   # Creating object
+
+car2 = Cars.new("Toyota Corola", "Toyota Corporation","Japan")        # Creating object
+puts car1.made_in       # Printing object value
