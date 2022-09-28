541b20035fd9b5588da563ba63b2190d72eb107f
ExpertZs <zs.freelancer1@gmail.com>
2022-09-28 17:43:15 +0600

    Creating If else

---
+++ conditional_loop_others.rb
@@ -0,0 +1,14 @@
+# If else conditional Statement
+  ishappy =true                                         # Initializing Booleans
+  isrubyprogrammer =  true                              # Initializing Booleans
+  if ishappy and isrubyprogrammer                       # If condition Started
+    puts "You are a Happy Ruby Programmeer"             # Print
+  elsif !ishappy and isrubyprogrammer                   # else If condition
+    puts "You are not a happy Ruby Programmeer"         # Print
+  elsif ishappy and !isrubyprogrammer                   # else If condition
+    puts "You are happy non Ruby Programmeer"           # Print
+  else                                                  # else
+    puts "You are neither happy nor Ruby Programmeer"   # Print
+  end                                                   # End of If else conditional Statement
+
+  
