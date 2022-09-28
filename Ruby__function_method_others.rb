105004f6b220f7c43ddeba01451f6e174ed990a8
ExpertZs <zs.freelancer1@gmail.com>
2022-09-28 10:52:46 +0600

    Variable initialization in ruby

--- Ruby__function_method_others.rb
+++ Ruby__function_method_others.rb
@@ -1,3 +1,11 @@
 #Printing system  in Ruby
 puts "私は　ザヒドゥル-エスラム　ともうします。"  #3printing with new line
 print "ザヒドゥルとようんでください。" #printing without new line
+puts "私は二十六さいです。"  #3printing with new line
+
+
+# Initializing variable in Ruby
+charecter_name  = "Zahidul islam"
+character_age  =  "26"
+puts ("Hey this is : " + charecter_name)
+puts  ("I am " + character_age + "years old")
