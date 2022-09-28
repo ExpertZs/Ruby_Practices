c17e96134dc3f4cea49ccb72276786bf93948037
ExpertZs <zs.freelancer1@gmail.com>
2022-09-28 17:54:49 +0600

    If else with max method

--- conditional_loop_others.rb
+++ conditional_loop_others.rb
@@ -11,4 +11,16 @@
     puts "You are neither happy nor Ruby Programmeer"   # Print
   end                                                   # End of If else conditional Statement
 
+  # If else in Method
+  def max(num1, num2, num3)                 # Initializing a method with 3 arguments
+    if num1>=num2 and num1>=num3            # If condition Started
+      return num1                           # return value
+    elsif num2>=num1  and num2>=num3        # else if condition Started
+      return num2                           # return value
+    else                                    # else  condition
+      return num3                           # return value
+    end                                     # End of IF else condition
+  end                                       # End of Method
+  puts max(5,20,6)                          # Print max by passing parameter value
+
   
